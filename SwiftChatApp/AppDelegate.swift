31316a05a949a52388589b2ce24c45adae9d731d
ゆまる <52074826+Youcircle13@users.noreply.github.com>
2019-07-11 11:51:32 +0900

    Update AppDelegate.swift

--- SwiftChatApp/AppDelegate.swift
+++ SwiftChatApp/AppDelegate.swift
@@ -18,25 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     /***** 【NCMB】APIキー *****/
     //let applicationkey = "YOUR_NCMB_APPLICATION_KEY"
     //let clientkey = "YOUR_NCMB_CLIENTKEY"
-    
-    let applicationkey = "88b626747d953fb6d3aefe54e88073c4237e18bd4bc493da5de771263577726b"
-    let clientkey = "104ac9817ee72e672a396105457db8e4d2892463b8e544bfa2fdc1e16aa42801"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         /***** 【NCMB】SDKの初期化 *****/
-        NCMB.initialize(
-            applicationKey: applicationkey,
-            clientKey: clientkey)
         
+        /***** 【NCMB】SDKの初期化 *****/
         return true
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
 
         /***** 【NCMB】会員管理 ログアウト *****/
-        NCMBUser.logOut()
-        
+
+        /***** 【NCMB】会員管理 ログアウト *****/
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
