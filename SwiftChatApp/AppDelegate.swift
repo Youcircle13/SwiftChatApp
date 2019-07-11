a10130be29f6fd054fe375ead5beefb2d230d320
ゆまる <52074826+Youcircle13@users.noreply.github.com>
2019-07-12 08:07:33 +0900

    Update AppDelegate.swift

--- SwiftChatApp/AppDelegate.swift
+++ SwiftChatApp/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NCMB.initialize(
             applicationKey: applicationkey,
             clientKey: clientkey)
-        
+        /***** 【NCMB】SDKの初期化 *****/
         return true
     }
     
@@ -36,6 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         /***** 【NCMB】会員管理 ログアウト *****/
         NCMBUser.logOut()
+        /***** 【NCMB】会員管理 ログアウト *****/
         
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
