54aaf407338bcc65aa72af7eb473d847f62978da
ゆまる <52074826+Youcircle13@users.noreply.github.com>
2019-07-16 08:59:59 +0900

    Update ChatRoomViewController.swift

--- SwiftChatApp/ChatRoomViewController.swift
+++ SwiftChatApp/ChatRoomViewController.swift
@@ -124,12 +124,11 @@ class ChatRoomViewController: JSQMessagesViewController {
     // image data for item
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
         
-        //senderId == 自分　だった場合表示しない
         let senderId = messages[indexPath.row].senderId
         
-        if senderId == user?.objectId {
-            return nil
-        }
+        /***** senderId == 自分　だった場合表示しない *****/
+
+        /***** senderId == 自分　だった場合表示しない *****/
         return JSQMessagesAvatarImage.avatar(with: UIImage(named: "user"))
     }
     
